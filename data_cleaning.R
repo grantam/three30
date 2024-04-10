e44f1de6b0ac287dd894d16a9ecb4daf4907d168
grantam <141366794+grantam@users.noreply.github.com>
2024-04-10 14:08:50 -0600

    cleaning code

--- data_cleaning.R
+++ data_cleaning.R
@@ -10,6 +10,6 @@ party <- read.csv("~/elements/data/partyinst_data.csv") %>%
   select(v2paid, psla, piendla, COWcode, year)
 dat <- left_join(party, vdem, by = c("year", "COWcode"))
 dat <- left_join(dat, vparty, by = c("year", "v2paid")) %>%
-  select(COWcode.x, country_name.x, year, v2x_polyarchy, v2elvaptrn, v2eltrnout, v2eltype_0, v2eltype_6, v2elcomvot, v2elparlel, v2xpa_antiplural, v2pavote, v2paseatshare, v2paelcont, v2papariah, piendla, psla)
+  select(COWcode.x, v2x_polyarchy, v2elvaptrn, v2eltrnout, v2eltype_0, v2eltype_6, v2elcomvot, v2elparlel, v2xpa_antiplural, v2pavote, v2paseatshare, v2paelcont, v2papariah, piendla, psla, v2pariglef)
 
 write.csv(dat, file = "partyinst_data_330.csv", row.names = FALSE)
