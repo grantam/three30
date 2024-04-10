83711e5384a087e6e802571e05eca63d136eda45
grantam <141366794+grantam@users.noreply.github.com>
2024-04-10 10:38:34 -0600

    cleaning code

---
+++ data_cleaning.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(ggthemes)
+library(ggfortify)
+library(GGally)
+library(vdemdata)
+
+
+party <- read.csv("~/elements/data/partyinst_data.csv") %>%
+  rename(v2paid = party_id) %>%
+  select(v2paid, psla, piendla, COWcode, year)
+dat <- left_join(party, vdem, by = c("year", "COWcode"))
+dat <- left_join(dat, vparty, by = c("year", "v2paid")) %>%
+  select(COWcode.x, country_name.x, year, v2x_polyarchy, v2elvaptrn, v2eltrnout, v2eltype_0, v2eltype_6, v2elcomvot, v2elparlel, v2xpa_antiplural, v2pavote, v2paseatshare, v2paelcont, v2papariah, piendla, psla)
+
+write.csv(dat, file = "partyinst_data_330.csv", row.names = FALSE)
